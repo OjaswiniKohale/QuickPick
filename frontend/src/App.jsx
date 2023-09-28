4d31dee6951daa452c20300f914c623a8d0f9d6b
OjaswiniKohale <ojaswini751@gmail.com>
2023-09-28 19:04:23 +0530

    Home Page Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Checkout from './pages/Checkout';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
 		<Router>
 			<Routes>
+				<Route path='/' element={<HomePage />}></Route>
 				<Route path='/login' element={<Login />}></Route>
 				<Route path='/signup' element={<SignUp />}></Route>
 				<Route path='/checkout' element={<Checkout />}></Route>
