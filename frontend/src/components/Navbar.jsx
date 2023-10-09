6eab277e785b9dd5ff6faa7b3841d7afa46563fe
OjaswiniKohale <ojaswini751@gmail.com>
2023-10-09 22:07:11 +0530

    buttons in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import QuickPickLogo from '../assets/quickPicklogo .png'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from 'react-router-dom';
 
 const StyledToolBar = styled(Toolbar)({
     display: "flex",
@@ -22,15 +23,21 @@ const Navbar = () => {
     return(
         <AppBar position='sticky' sx={{backgroundColor: "white"}}>
             <StyledToolBar>
+                <Link to="/">
                 <img src={QuickPickLogo} style={{height:"4rem", marginLeft:"-1.5rem"}}/>
+                </Link>
                 <Search sx={{color:"black", display:"flex", justifyContent:"flexstart", alignItems: "center" }}><InputBase placeholder='search...' sx={{width: "100%"}}/></Search>
                 <Icons>
+                <Link to="/Cart">
                     <Badge badgeContent={4} color="primary" sx={{ marginRight: "1.2rem", fontSize: "4rem"}}>
                         <ShoppingCartIcon color="action" sx={{fontSize: "2rem", color: "#F57F17" }}/>
                     </Badge>
+                </Link>
+                <Link to="/login">
                     <Badge sx={{fontSize: "2rem"}}>
                         <AccountCircleIcon color="action" sx={{fontSize: "2rem", color: "#F57F17" }}/>
                     </Badge>
+                </Link>
                 </Icons>
             </StyledToolBar>
         </AppBar>
