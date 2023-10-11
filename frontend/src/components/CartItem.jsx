8e3bfc34e51da82556919064d96ae70b82359f0d
parshva555 <parshvadani@gmail.com>
2023-10-11 14:25:05 +0530

    Updated CartPage and CartItem component

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,18 +1,73 @@
 import React from 'react'
-
-export const CartItem = (props) => {
-    const url = props.data;
-
+import { useState } from 'react';
+ const CartItem = (props) => {
+    const { name, price, img } = props;
+    const [quantity, setQuantity] = useState(1);
+  
+    const total = (quantity * price).toFixed(2);
+  
+    const increaseQuantity = () => {
+      setQuantity(quantity + 1);
+    };
+  
+    const decreaseQuantity = () => {
+      if (quantity > 1) {
+        setQuantity(quantity - 1);
+      }
+    };
+  
+    const removeFromCart = () => {
+      setQuantity(0);
+    };
     return (
-        <div className='cartItem'>
-            <img src={url} className='product-image'/>
-            <div className='description'>
-                <p>
-                    {" "}
-                    <b> Orange </b>
-                </p>
-                <p>$100</p>
-            </div>
+      <div className="card border border-3 rounded" style={{ width: "18rem", height: "100%" }}>
+        <img
+          src={img}
+          className="card-img-top"
+          alt={name}
+          style={{ maxHeight: "150px", objectFit: "cover" }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text text-muted">Rs.{price.toFixed(2)} per item</p>
         </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <div className="d-flex justify-content-between align-items-center">
+              <span>Quantity:&nbsp;&nbsp;</span>
+              <div className="input-group">
+                <button
+                  className="btn btn-outline-danger btn-sm"
+                  onClick={decreaseQuantity}
+                >
+                  -
+                </button>
+                <span className="input-group-text">{quantity}</span>
+                <button
+                  className="btn btn-outline-success btn-sm"
+                  onClick={increaseQuantity}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </li>
+          <li className="list-group-item">
+            <div className="d-flex justify-content-between align-items-center">
+              <span>Total:</span>
+              <strong>Rs.{total}</strong>
+            </div>
+          </li>
+        </ul>
+        <div className="card-footer d-flex justify-content-between">
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={removeFromCart}
+          >
+            Remove
+          </button>
+        </div>
+      </div>
     );
 }
+export default CartItem;
--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,33 +1,33 @@
-import React, {useContext} from "react";
-import {CartItem} from "../components/CartItem";
-import "../styles/cart.css"
-const PRODUCTS=[
-    "https://tse4.mm.bing.net/th?id=OIP.IXKCpF-CG8jGSmOdUoYKIQHaEK&pid=Api&P=0&h=180", 
-    "https://tse1.mm.bing.net/th?id=OIP.S0MwlWV6Tgy2br4GfBaJcgHaE6&pid=Api&P=0&h=180",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1aXqHdPUhiGbV6IpAjNQWfago64IWwCZqlA&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcPhPFg7QhdbFjttG9SNME-uHndIPW2Y40Zw&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKNw11AtnkYWJNc3efgIMjnzsXPAB19qAP_w&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2dQwLWdyqVnvijep83uzmRoX-FGXO_9SUcw&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfXdJPRyEebLRiUikB5x9hw-AwOYdNLUaKeA&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8Q4uIH9CEAuEL80813qdvOdWUGUzbpbYMxQ&usqp=CAU"
-  ]
+import React from "react";
+import "../styles/cart.css";
+import CartItem from '../components/CartItem';
 
-  export const Cart = () => {
+const PRODUCTS = [
+  "https://tse4.mm.bing.net/th?id=OIP.IXKCpF-CG8jGSmOdUoYKIQHaEK&pid=Api&P=0&h=180",
+  "https://tse1.mm.bing.net/th?id=OIP.S0MwlWV6Tgy2br4GfBaJcgHaE6&pid=Api&P=0&h=180",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1aXqHdPUhiGbV6IpAjNQWfago64IWwCZqlA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcPhPFg7QhdbFjttG9SNME-uHndIPW2Y40Zw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKNw11AtnkYWJNc3efgIMjnzsXPAB19qAP_w&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2dQwLWdyqVnvijep83uzmRoX-FGXO_9SUcw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfXdJPRyEebLRiUikB5x9hw-AwOYdNLUaKeA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8Q4uIH9CEAuEL80813qdvOdWUGUzbpbYMxQ&usqp=CAU",
+];
 
-
-    return (
-        <div className="cart">
-            <div>
-                <h1>Your Cart Items</h1>
-            </div>
-            <div className="cartItems">
-                {PRODUCTS.map((product,index) => {
-                    
-                        return <CartItem data={product} key={index}/>; 
-                                    })}
-            </div>
-        </div>
-    )
-  }
+export const Cart = () => {
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="row">
+        {PRODUCTS.map((product, index) => (
+          <div className="col-md-3 mt-5" key={index}>
+            <CartItem price={30} name="Banana" img={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
