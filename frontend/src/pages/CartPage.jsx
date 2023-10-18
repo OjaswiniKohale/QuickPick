257295543401ef8cc774c77b6f187ff624846738
parshva555 <parshvadani@gmail.com>
2023-10-18 19:02:34 +0530

    Added total cart functionality

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,13 +1,78 @@
+// import React, { useState } from "react";
+// // import { useNavigate } from "react-router-dom";
+// import "../styles/cart.css";
+// import CartItem from "../components/CartItem";
+
+// // const navigate = useNavigate();
+
+// export const CartPage = ({ currentCart, makeCart }) => {
+//   const updateQuantity = (name, newQuantity) => {
+//     const updatedCart = currentCart.map((product) => {
+//       if (product.name === name) {
+//         return {
+//           ...product,
+//           quantity: newQuantity, // Update the quantity
+//         };
+//       }
+//       return product;
+//     });
+//     makeCart(updatedCart);
+//   };
+
+//   const removeFromCart = (name) => {
+//     const existingProduct = currentCart.find(
+//       (product) => product.name === name,
+//     );
+//     let updatedCart;
+//     if (existingProduct) {
+//       updatedCart = [...currentCart];
+
+//       updatedCart = updatedCart.filter(
+//         (item) => item.name !== existingProduct.name,
+//       );
+//       makeCart(updatedCart);
+//     }
+//   };
+//   return (
+//     <div className="cart ">
+//       <div>
+//         <h1>Your Cart Items</h1>
+//       </div>
+//       <div className="container ml-3">
+//         <div className="row">
+//           {currentCart.map((product, index) => (
+//             <div
+//               className={`col-md-${
+//                 currentCart.length < 3 ? 6 : currentCart.length === 3 ? 4 : 3
+//               } mt-5 `}
+//               key={index}
+//             >
+//               <CartItem
+//                 price={product.price}
+//                 name={product.name}
+//                 img={product.img}
+//                 quantity={product.quantity}
+//                 setQuantity={updateQuantity}
+//                 removeFromCart={removeFromCart}
+//               />
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+
+
+//     </div>
+//   );
+// };
+
+// export default CartPage;
 import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
 import "../styles/cart.css";
 import CartItem from "../components/CartItem";
 
-// const navigate = useNavigate();
-
 export const CartPage = ({ currentCart, makeCart }) => {
   const updateQuantity = (name, newQuantity) => {
-    const updatedCart = currentCart.map((product) => {
+       const updatedCart = currentCart.map((product) => {
       if (product.name === name) {
         return {
           ...product,
@@ -20,7 +85,7 @@ export const CartPage = ({ currentCart, makeCart }) => {
   };
 
   const removeFromCart = (name) => {
-    const existingProduct = currentCart.find(
+        const existingProduct = currentCart.find(
       (product) => product.name === name,
     );
     let updatedCart;
@@ -33,8 +98,18 @@ export const CartPage = ({ currentCart, makeCart }) => {
       makeCart(updatedCart);
     }
   };
+
+  // Calculate the total cost
+  const calculateTotalCost = () => {
+    let totalCost = 0;
+    for (const product of currentCart) {
+      totalCost += product.quantity * product.price;
+    }
+    return totalCost;
+  };
+
   return (
-    <div className="cart ">
+    <div className="cart">
       <div>
         <h1>Your Cart Items</h1>
       </div>
@@ -60,25 +135,12 @@ export const CartPage = ({ currentCart, makeCart }) => {
         </div>
       </div>
 
-      {/* {totalAmount > 0 ? (
-        <div className="checkout">
-          <p> Subtotal: ${totalAmount} </p>
-          <button onClick={() => navigate("/")}> Continue Shopping </button>
-          <button
-            onClick={() => {
-              checkout();
-              navigate("/checkout");
-            }}
-          >
-            {" "}
-            Checkout{" "}
-          </button>
-        </div>
-      ) : (
-        <h1> Your Shopping Cart is Empty</h1>
-      )} */}
+      <div className="total-cost">
+        Total Cost: ${calculateTotalCost().toFixed(2)}
+      </div>
     </div>
   );
 };
 
 export default CartPage;
+
